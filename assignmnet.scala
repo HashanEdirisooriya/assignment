cf795549c34a1da565029d62b5c7dcb1c72ec3f1
HashanEdirisooriya <65823741+HashanEdirisooriya@users.noreply.github.com>
2020-05-24 23:58:44 +0530

    Add files via upload

---
+++ assignmnet.scala
@@ -0,0 +1,15 @@
+
+import scala.math.{Pi}
+
+object assignmnet {
+  def main(args: Array[String]){
+
+      
+    println(" Fahrenheit- " + 35 * 1.8F + 32.0F)
+    println("volume- " + 5*5*5 * 4/3 * 22/7)
+    println("Wholesale Cost- " + 60 * 24.95F * 60/100 + 50*3 + (60 - 50)*0.75F)
+    
+        
+    }
+  
+}
